358fb400931798272a2f4dd2eed2cfb33754f271
APS4087 <aungpyaesoe.bgs@Gmail.com>
2023-08-22 10:48:01 +0800

    added register form

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .forms import signUpForm
 # Create your views here.
 def home(request):
     
@@ -26,3 +26,21 @@ def logout_user(request):
 	logout(request)
 	messages.success(request, 'You have been loggout.....')
 	return redirect('home')
+
+def register_user(request):
+	if request.method == 'POST':
+		form = signUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password = password)
+			login(request, user)
+			messages.success(request, 'You have successfully registered...')
+			return redirect('home')
+	else:
+		form = signUpForm()
+		return render(request, 'register.html', {'form':form})
+	return render(request, 'register.html', {'form':form})
+
