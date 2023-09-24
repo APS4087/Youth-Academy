119fa984ad9bc70fc89e2e36410cfcd34531e44a
APS4087 <aungpyaesoe.bgs@Gmail.com>
2023-09-24 20:40:29 +0800

    stu & staff management added

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,15 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('login/', views.login_user, name = 'login'),
+    
+    path('', views.land_page, name='land_page'),
+    
+    path('login_page/', views.login_page, name='login_page'),
+    path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name = 'logout'),
     path('register/', views.register_user, name = 'register'),
+    path('staff_list/', views.staff_list_page, name = 'staff_list'),
+    path('stu_list/', views.stu_list_page, name = 'stu_list'),
     path('edit/<int:id>/', views.edit, name='edit'),
     path('delete/<int:id>/', views.delete, name='delete'),
     path('<int:id>', views.view_staff, name='view_staff'),
--- main/views.py
+++ main/views.py
@@ -8,10 +8,10 @@ from django.db.models import F
 from django.core.validators import EmailValidator
 from django.core.exceptions import ValidationError
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
-def home(request):
 	
-    
+def login_user(request):
 	if request.method == 'POST':
 		email = request.POST['email']
 		
@@ -23,35 +23,62 @@ def home(request):
 			email_validator(email)
 		except ValidationError:
 			messages.error(request, "Please enter a valid email address.")
-			return redirect('home')
+			return redirect('login.html')
 		try:
             # Attempt to retrieve the user by email (case-insensitive)
 			user = User.objects.get(email__iexact=email)
 		except User.DoesNotExist:
 			messages.error(request, "User with this email does not exist.")
-			return redirect('home')
+			return redirect('login_page')
 		# Authenticate
 		user = authenticate(request, username=user.username, password=password)
 		if user is not None:
 			login(request, user)
 			messages.success(request, "You Have Been Logged In!")
-			return redirect('home')
+			return redirect('staff_list')
 		else:
 			messages.success(request, "There Was An Error Logging In, Please Try Again...")
-			return redirect('home')
+			return redirect('login_page')
 	else:
-		staffs_and_details = User.objects.select_related('account')
-		context = {'staffs_and_details': staffs_and_details}
-		return render(request, 'home.html', context)
+		staffs = User.objects.select_related('account')
+		admin_and_teacher_staffs = staffs.filter(account__user_type__in=['admin', 'teacher'])
+		context = {'staffs': admin_and_teacher_staffs}
+		return render(request, 'staff_list_display.html', context)
+
+
+
+def staff_list_page(request):
+    if request.user.is_authenticated:
+        staffs = User.objects.select_related('account')
+        admin_and_teacher_staffs = staffs.filter(account__user_type__in=['admin', 'teacher'])
+        context = {'staffs': admin_and_teacher_staffs}
+        return render(request, 'staff_list_display.html', context)
+    else:
+        # Redirect to the login page or display an error message
+        return redirect('login')
 	
-def login_user(request):
+def stu_list_page(request):
+    if request.user.is_authenticated:
+        students = User.objects.select_related('account')
+        student_details = students.filter(account__user_type__in=['student'])
+        context = {'students': student_details}
+        return render(request, 'students_list_display.html', context)
+    else:
+        # Redirect to the login page or display an error message
+        return redirect('login')
+
+
+def login_page(request):
 	return render(request, 'login.html')
+
+def land_page(request):
+    return render(request, 'landpage.html')
 
 
 def logout_user(request):
 	logout(request)
 	messages.success(request, 'You have been loggout.....')
-	return redirect('home')
+	return redirect('land_page')
 
 def register_user(request):
 	if request.method == 'POST':
@@ -65,26 +92,20 @@ def register_user(request):
 				user_type = form.cleaned_data['user_type'],
 			)
 			user_profile.save()
-			authenticate(username=user.username, password = user.password)
-			login(request, user)
-			messages.success(request, 'You have successfully registered...')
-			return redirect('home')
+			
+			messages.success(request, 'You have successfully registered...Please login with your credentials...')
+			return redirect('login_page')
 	else:
 		form = signUpForm()
 		return render(request, 'register.html', {'form':form})
 	return render(request, 'register.html', {'form':form})
 
-def staffDashboard(request):
-	return render(request, 'staffDashboard.html',{
-		'staff' : User.objects.all()
-	})
 
 
 def view_staff(request, id):
-  return HttpResponseRedirect(reverse('home'))
+  return HttpResponseRedirect(reverse('land_page'))
 
 def edit(request, id):
-	
     staff = User.objects.get(pk=id)
 
     if request.method == 'POST':
@@ -98,20 +119,30 @@ def edit(request, id):
             user_profile.user_type = form.cleaned_data['user_type']
             user_profile.save()
 
-            # Authenticate and log in the user
-            authenticate(username=user.username, password=user.password)
-            login(request, user)
-
             messages.success(request, 'You have successfully edited...')
-            return redirect('home')
+
+            # Determine the user type and redirect accordingly
+            if user_profile.user_type in ['staff', 'admin']:
+                return redirect('staff_list')
+            elif user_profile.user_type == 'student':
+                return redirect('stu_list')
     else:
         form = userEditForm(instance=staff)
 
     return render(request, 'staffEdit.html', {'form': form})
 
 def delete(request, id):
-  if request.method == 'POST':
-    staff = User.objects.get(pk=id)
-    staff.delete()
-  messages.success(request, staff.first_name+" has been deleted")
-  return HttpResponseRedirect(reverse('home'))
+    if request.method == 'POST':
+        staff = User.objects.get(pk=id)
+        user_profile = Account.objects.get(user=staff)
+        staff.delete()
+
+        messages.success(request, staff.first_name + " has been deleted")
+
+        # Determine the user type and redirect accordingly
+        if user_profile.user_type in ['staff', 'admin']:
+            return redirect('staff_list')
+        elif user_profile.user_type == 'student':
+            return redirect('stu_list')
+
+    return HttpResponseRedirect(reverse('staff_list'))
